1f303b6fd68b0d156f91825c39a521b4f88644b4
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-07-02 20:24:08 +0530

    add some comment to the code

--- App.js
+++ App.js
@@ -13,8 +13,8 @@ const message = 'Hello World'
 function App() {
   return (
     <View>
-    <Text style = {{fontSize: 30, marginTop: 30, textAlign: 'center', color: 'red'}}>{message}</Text> // This is known as inline styling of JSX.
-    </View>
+    <Text style = {{fontSize: 30, marginTop: 30, textAlign: 'center', color: 'red'}}>{message}</Text> 
+    </View> // This is known as inline styling of JSX.
   );
 }
 
